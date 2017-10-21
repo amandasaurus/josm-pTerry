e483e2bc46c9c9a651bea1f2d57da90d8526a55f
Rory McCann <rory@technomancy.org>
2017-10-21 14:17:41 +0200

    remove uneded

--- src/org/technomancy/openstreetmap/pterry/pTerryPlugin.java
+++ src/org/technomancy/openstreetmap/pterry/pTerryPlugin.java
@@ -1,23 +1,7 @@
 // License: GPL. For details, see LICENSE file.
 package org.technomancy.openstreetmap.pterry;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.UploadAction;
-import org.openstreetmap.josm.data.osm.INode;
-import org.openstreetmap.josm.data.osm.IPrimitive;
-import org.openstreetmap.josm.data.osm.IRelation;
-import org.openstreetmap.josm.data.osm.IWay;
-import org.openstreetmap.josm.data.osm.OsmPrimitive;
-import org.openstreetmap.josm.data.osm.visitor.PrimitiveVisitor;
-import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
-import org.openstreetmap.josm.io.OsmReader;
-import org.openstreetmap.josm.io.OsmServerWriter;
-import org.openstreetmap.josm.io.OsmWriterFactory;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
--- src/org/technomancy/openstreetmap/pterry/pTerryUploadHook.java
+++ src/org/technomancy/openstreetmap/pterry/pTerryUploadHook.java
@@ -1,18 +1,9 @@
 // License: GPL. For details, see LICENSE file.
 package org.technomancy.openstreetmap.pterry;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 
 import org.openstreetmap.josm.actions.upload.UploadHook;
-import org.openstreetmap.josm.data.APIDataSet;
-import org.openstreetmap.josm.data.osm.INode;
-import org.openstreetmap.josm.data.osm.IPrimitive;
-import org.openstreetmap.josm.data.osm.IRelation;
-import org.openstreetmap.josm.data.osm.IWay;
-import org.openstreetmap.josm.data.osm.OsmPrimitive;
 
 public class pTerryUploadHook implements UploadHook {
     @Override
